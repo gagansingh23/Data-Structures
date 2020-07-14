3b78753a333505fb09546f319de948002c2c1750
gagansingh23 <gsinghpnjb@gmail.com>
2020-07-14 02:13:54 -0400

    added 2nd test for stack and queue files

--- queue/queue.py
+++ queue/queue.py
@@ -1,3 +1,4 @@
+from singly_linked_list import LinkedList
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
@@ -12,17 +13,44 @@ return elements in First In First Out order.
    
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
-"""
+# """
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.queue = []
+        
+#     def __len__(self):
+#         return len(self.queue)
+
+#     def enqueue(self, value):
+#         self.queue.append(value)
+
+
+#     def dequeue(self):
+#         if self.queue == []:
+#             return None
+#         return self.queue.pop(0)
+        
+
+
+# enqueue adds an element to the back of the queue.
+# dequeue removes and returns the element at the front of the queue.
+# len returns the number of elements in the queue.
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.queue = LinkedList()
+
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.queue.add_to_tail(value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.queue.remove_head()
--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,4 @@
+from singly_linked_list import LinkedList
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -10,16 +11,61 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+# class Stack:
+#     def __init__(self):
+#         self.items = []
+        
+
+#     def __len__(self):
+#         return len(self.items)
+        
+
+#     def push(self, value):
+#         self.items.append(value)
+
+#     def pop(self):
+#         if self.items.__len__() == 0:
+#             pass
+#         else:
+#             value = self.items[len(self.items) - 1]
+#             self.items.pop()
+#             return value
+
+
+
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.items = LinkedList()
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.items.add_to_tail(value)
+        self.size += 1
 
     def pop(self):
-        pass
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.items.remove_tail()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
